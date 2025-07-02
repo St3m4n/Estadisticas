dd9a2babb1703b10ddf2b86008100ad286e41714
Pato <patricio.zetazeta@gmail.com>
2025-07-02 01:41:45 -0400

    Elimino paquete con mayúscula y dejo sólo 'estadisticas'

--- src/main/java/com/edutech/Estadisticas/model/TipoReporte.java
+++
@@ -1,7 +0,0 @@
-package com.edutech.estadisticas.model;
-
-public enum TipoReporte {
-    ESTUDIANTES_INSCRITOS,
-    PROGRESO_ESTUDIANTES,
-    RENDIMIENTO_SECCIONES
-}
--- src/main/java/com/edutech/Estadisticas/repository/ReporteRepository.java
+++
@@ -1,8 +0,0 @@
-package com.edutech.estadisticas.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.edutech.estadisticas.model.Reporte;
-
-public interface ReporteRepository extends JpaRepository<Reporte, Long> {
-}
